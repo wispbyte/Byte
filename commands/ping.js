b83643b5aa3b531aea7e4d7d66a2b6666651b878
mynameisbob1928 <129603125+mynameisbob1928@users.noreply.github.com>
2025-03-16 21:02:28 +0000

    Prefix command example, the rest will come soon

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,5 @@
 const { formatTime } = require('#functions');
-const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
-const { timeToString } = require('#functions');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,13 +11,13 @@ module.exports = {
      * @param {import('discord.js').Client} client
      */
 	async execute(interaction, client) {
-		await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+		await interaction.deferReply();
 		const uptime = process.uptime();
 		const formattedTime = formatTime(Math.floor(uptime) * 1000, '%dd %hh %mm %ss');
 
 		const memoryUsageMb = (process.memoryUsage().rss / 1024 / 1024).toFixed(1);
 
-		await interaction.editReply({
+		interaction.editReply({
 			embeds: [new EmbedBuilder()
 				.setTitle('Bot Information')
 				.setColor('Aqua')
@@ -41,6 +40,45 @@ module.exports = {
 					},
 				]).setFooter({ iconURL: interaction.inGuild() ? interaction.guild.iconURL() : undefined, text: 'WispByte' }),
 			],
+		});
+	},
+	/**
+	 * @param {import('discord.js').Message} message
+	 * @param {string[]} args
+	 * @param {import('../main').ByteClient} client
+	 */
+	async messageExecute(message, args, client) {
+		const reply = await message.reply('Pinging...');
+
+		const uptime = process.uptime();
+		const formattedTime = formatTime(Math.floor(uptime) * 1000, '%dd %hh %mm %ss');
+
+		const memoryUsageMb = (process.memoryUsage().rss / 1024 / 1024).toFixed(1);
+
+		reply.edit({
+			embeds: [new EmbedBuilder()
+				.setTitle('Bot Information')
+				.setColor('Aqua')
+				.setTimestamp(Date.now())
+				.addFields([
+					{
+						name: `${client.cemojis.system.latency.emoji} API Latency`,
+						value: `> \`${client.ws.ping}ms\``,
+						inline: true,
+					},
+					{
+						name: `${client.cemojis.system.uptime.emoji} Uptime`,
+						value: `> \`${formattedTime}\``,
+						inline: true,
+					},
+					{
+						name: `${client.cemojis.system.memory.emoji} Memory usage`,
+						value: `> \`${memoryUsageMb}MB\``,
+						inline: true,
+					},
+				]).setFooter({ iconURL: message.inGuild() ? message.guild.iconURL() : undefined, text: 'WispByte' }),
+			],
+			content: '',
 		});
 	},
 };
